819d7fe3a10e077d796767e4aa3840492425e387
Mohd Zeeshan <100269606+MohdZeeshan11@users.noreply.github.com>
2023-01-16 15:36:10 +0530

    error fixed

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -5,56 +5,26 @@ const validateToken = asyncHandler(async(req,res,next)=>{
     let token;
 
     let authHeader = req.headers.Authorization || req.headers.authorization
-
-    // if(authHeader && authHeader.startsWith('Bearer')){
-    //     token = authHeader.split(" ")[1];
-    // console.log('authHeader = ',authHeader);
         if(authHeader){
         //     token = authHeader.split(" ")[1];
-        token = authHeader
+        token = authHeader;
+        // if(token)
+        console.log('token = ',token);
         jwt.verify(token,process.env.JWT_SIGNING_KEY,(err,decoded)=>{
             if(err){
-                res.status(401);
-                throw new Error("user is not authorized");
+                res.status(401).json("user is not authorized");
+                // throw new Error("user is not authorized");
+                return;
             }
             console.log("decoded",decoded)
             // req.user = decoded.user
             // next()
+            next()
         })
-        if(!token){
-            res.status(401);
-                throw new Error("user is not authorized or token missing");
-        }
-        next()
+    }
+    if(!token){
+        res.status(401).json("user is not authorized or token missing");
     }
 })
 
 module.exports = validateToken;
-
-
-
-// export const authUser = async (req, res, next) => {
-//     const { authorization: token } = req.headers;
-//     const user = getUserFromAccessToken(token);
-//     if(user.isApiUser){
-//         throw new UnAuthenticatedUser(401, "Authentication failed. Api user not allowed.", "unauthenticated-user" );
-//     }
-//     if(!user){
-//         throw new UnAuthenticatedUser(401, "Authentication failed. Try logging in again.", "unauthenticated-user" );
-//     } else {
-//         req.user = user;
-//     }
-//     next();
-// };
-
-
-// export const getUserFromAccessToken = (accessToken) => {
-//     let data;
-//     try {
-//         const token = jwt.verify(accessToken, process.env.JWT_SIGNING_KEY);
-//         data = token.data;
-//     } catch (e) {
-//         console.error("JWT Verification failed.")
-//     }
-//     return data;
-// };
